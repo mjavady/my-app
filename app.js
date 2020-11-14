c9bb9aea138b805d74bf77ccc054e73199d9a1d1
unknown <javady1997@gmail.com>
2020-11-14 15:39:49 +0330

    didn't save\

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/Failure", (req,res)=>{
     res.redirect("/");
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("server is running on port 3000.");
 })
